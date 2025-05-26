f7aa6e51cc8cd5f4467980669f372b1a662ccde6
23R11A6719 <23r11a6719@gcet.edu.in>
2025-05-26 19:27:14 +0530

    update

--- src/components/App14.jsx
+++ src/components/App14.jsx
@@ -1,30 +1,45 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+
 export default function App14() {
   const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
   const fetchData = async () => {
     const url = "https://jsonplaceholder.typicode.com/users";
     const res = await axios(url);
     setUsers(res.data);
   };
+
   useEffect(() => {
     fetchData();
   }, []);
+
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
       <h1>App14</h1>
-      <h2>useEffect hook to fetch api</h2>
+      <h2>useEffect hook to fetch API</h2>
       <p>
-        <input type="text" placeholder="Search name" />
+        <input
+          type="text"
+          placeholder="Enter to search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
       </p>
       <ol>
-        {users &&
-          users.map((value) => (
-            <li key={value.id}>
-              {value.name}, {value.email}, {value.phone}
-            </li>
-          ))}
+        {filteredUsers.map((user) => (
+          <li key={user.id}>
+            <strong>ID:</strong> {user.id} <br />
+            <strong>Name:</strong> {user.name} <br />
+            <strong>Email:</strong> {user.email} <br />
+            <strong>Phone:</strong> {user.phone}
+          </li>
+        ))}
       </ol>
     </div>
   );
